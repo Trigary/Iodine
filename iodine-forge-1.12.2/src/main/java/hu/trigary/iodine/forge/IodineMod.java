69997e1025b3e46b58b93c47775754c08db327f3
Trigary <trigary0@gmail.com>
2019-08-12 16:19:35 +0200

    WIP attempt at making multiple mod versions

---
+++ iodine-forge-1.12.2/src/main/java/hu/trigary/iodine/forge/IodineMod.java
@@ -0,0 +1,6 @@
+package hu.trigary.iodine.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "iodine", useMetadata = true, clientSideOnly = true)
+public class IodineMod {}
---
+++ iodine-forge-1.13.2/src/main/java/hu/trigary/iodine/forge/IodineMod.java
@@ -0,0 +1,6 @@
+package hu.trigary.iodine.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("iodine")
+public class IodineMod {}
